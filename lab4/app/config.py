ab486f28e4cc98ced912623bea20f2981e0b43d8
анастасия <146527663+nastoiika@users.noreply.github.com>
2025-05-28 14:10:24 +0300

    Delete lab4/app directory

--- lab4/app/config.py
+++
@@ -1,6 +0,0 @@
-SECRET_KEY='9a5f2c7b6e9d4ab998a0b04f967cf6d8a1d2b36cb354f4dfbb0eb2e0c7e2e5c1'
-
-MYSQL_USER = 'nastoiika'
-MYSQL_PASSWORD = 'u7o9p0p0q1q1'
-MYSQL_HOST = 'nastoiika.mysql.pythonanywhere-services.com'
-MYSQL_DATABASE = 'nastoiika$super_database'
--- lab4/app/database.py
+++
@@ -1,3 +0,0 @@
-from .db import DBConnector
-
-db = DBConnector()
